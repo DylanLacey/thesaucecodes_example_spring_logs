b16fa50835cd5b7287ac713d870a909c796572d9
Dylan Lacey <dev@dylanlacey.com>
2021-10-05 13:28:54 +1000

    Added Profile Controller Test

--- src/main/java/com/saucelabs/community/spring_login/MvcConfig.java
+++ src/main/java/com/saucelabs/community/spring_login/MvcConfig.java
@@ -1,11 +1,16 @@
 package com.saucelabs.community.spring_login;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    private static final Logger logger = LoggerFactory.getLogger(MvcConfig.class);
+
     public void addViewControllers(ViewControllerRegistry registry) {
+        logger.info("adding Controllers");
         registry.addViewController("/profile").setViewName("profile");
         registry.addViewController("/").setViewName("home");
 //        registry.addViewController("/hello").setViewName("hello");
